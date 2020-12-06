728809345959dd0b682d5327f5c14edffe3d65d0
Andreas Reinle <gras64@web.de>
2020-12-06 01:53:56 +0100

    initial

--- __init__.py
+++ __init__.py
@@ -1,14 +1,31 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 
 
 class MirrorMirror(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('mirror.mirror.intent')
-    def handle_mirror_mirror(self, message):
-        self.speak_dialog('mirror.mirror')
+    def initialize(self):
+        self.gender_value = self.translate_namedvalues('gender.value')
 
+    @intent_handler(IntentBuilder("handlermirror").require("mirror").
+                    one_of("beautiful", "bad").optionally("man").optionally("woman").optionally("location").build())
+    def handle_mirror_mirror(self, message):
+        if message.data.get("man", False):
+            gender = "male"
+        elif message.data.get("woman", False):
+            gender = "female"
+        else:
+            gender = ""
+        gender = self.gender_value[gender]
+        if message.data.get("beautiful", False):
+            self.speak_dialog('friendly', data={"gender":gender, "beautiful":self.translate("beautiful", data=None)})
+        else:
+            self.speak_dialog('unfriendly', data={"gender":gender, "bad" :self.translate("bad", data=None)})
+
+    def shutdown(self):
+        super(MirrorMirror, self).shutdown()
 
 def create_skill():
     return MirrorMirror()
