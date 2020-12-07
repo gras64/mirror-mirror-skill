b8a0066af87fd34ccfbe3278fabaf5e0f7fb8600
Andreas Reinle <gras64@web.de>
2020-12-07 01:52:59 +0100

    add random answers friendly or not

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/smile-wink.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Mirror Mirror
-You probably know snow white now mayroft can say how beautiful you are
+You probably know snow white now mayroft can say how beautiful you are.
 
 ## About
-
+the skill can compliment you but can also be angry with you. it is a modern version of the grimm fairy tale snow white. in random mode in the settings there is also a negative answer.
 
 ## Examples
-* "Mirror mirror on the wall who is the most beautiful in the whole country"
+* "Mirror Mirror on the wall who is the most beautiful in the whole country"
+* "Mittor Mirror who is the biggest looser"
+* "Mirror Mirror who is the greatest"
 
 ## Credits
 @gras64
--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from adapt.intent import IntentBuilder
+import random
 
 
 class MirrorMirror(MycroftSkill):
@@ -8,6 +9,7 @@ class MirrorMirror(MycroftSkill):
 
     def initialize(self):
         self.gender_value = self.translate_namedvalues('gender.value')
+        
 
     @intent_handler(IntentBuilder("handlermirror").require("mirror").
                     one_of("beautiful", "bad").optionally("man").optionally("woman").optionally("location").build())
@@ -39,9 +41,12 @@ class MirrorMirror(MycroftSkill):
             gender = ""
         gender = self.gender_value[gender]
         if message.data.get("beautiful", False):
-            self.speak_dialog('friendly', data={"gender":gender, "beautiful":beautiful})
+            l = ["friendly"]
+            if self.settings.get('random', True):
+                l = ["friendly", "the.second"]
+            self.speak_dialog(random.choice(l), data={"gender":gender, "beautiful":beautiful, "location":location})
         else:
-            self.speak_dialog('unfriendly', data={"gender":gender, "bad" :bad})
+            self.speak_dialog('unfriendly', data={"gender":gender, "bad":bad, "location":location})
 
     def shutdown(self):
         super(MirrorMirror, self).shutdown()
